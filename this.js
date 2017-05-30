239bc9471c4a9cd278b8bd4ce98c03203e8d4fe9
Joe Garrison <jagarrison@saleae.com>
2017-05-30 09:06:08 -0700

    Update this.js

--- this.js
+++ this.js
@@ -1,17 +1,23 @@
-
+console.log("\nQuestion 1:");
 console.log(this);  
-// What is on the console? For Node?  For the Browser?
+//(a) When running in the Browser, what will be on the console here?
+//(b) When running in Node, what will be on the console here?
 
+console.log("\nQuestion 2:");
 var abc = 123;
 console.log(this.abc);
-// What is on the console? For Node?  For the Browser?
+//(a) When running in the Browser, what will be on the console here?
+//(b) When running in Node, what will be on the console here?
 
+console.log("\nQuestion 3:");
 function f1(){
     console.log(this);
 }
 f1(); 
-// What is on the console? For Node?  For the Browser?
+//(a) When running in the Browser, what will be on the console here?
+//(b) When running in Node, what will be on the console here?
 
+console.log("\nQuestion 4:");
 var obj = {
     foo: "123",
     f3: function(){
@@ -19,17 +25,21 @@ var obj = {
     }
 };
 obj.f3();
-// What is on the console?
+//(a) When running in the Browser, what will be on the console here?
+//(b) When running in Node, what will be on the console here?
 
+console.log("\nQuestion 5:");
 obj.f3.call({});
 // What is on the console?
 
+console.log("\nQuestion 6:");
 function f4(){
     console.log(this);
 }
 new f4();
 // What is on the console?
 
+console.log("\nQuestion 7:");
 this.xyz = 0;
 var obj2 = {
     xyz: 1,
@@ -40,10 +50,12 @@ var obj2 = {
 obj2.f5();
 // What is on the console?
 
+console.log("\nQuestion 8:");
 var f6 = obj2.f5.bind(this);
 f6();
 // What is on the console?
 
+console.log("\nQuestion 9:");
 obj2.f7 = () => { console.log( this.xyz ) };
 obj2.f7();
 // What is on the console?
